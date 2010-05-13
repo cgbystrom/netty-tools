fe931279fe1629efc397396851ef2a2b272c3c20
Carl Bystrom <cgbystrom@gmail.com>
2010-05-13 22:45:36 +0200

    Added simple channel pipeline for Thrift servers

---
+++ src/main/java/se/cgbystrom/netty/thrift/ThriftPipelineFactory.java
@@ -0,0 +1,30 @@
+package se.cgbystrom.netty.thrift;
+
+import org.jboss.netty.channel.ChannelPipeline;
+import org.jboss.netty.channel.ChannelPipelineFactory;
+import org.jboss.netty.handler.codec.frame.LengthFieldBasedFrameDecoder;
+import org.jboss.netty.handler.codec.frame.LengthFieldPrepender;
+
+import static org.jboss.netty.channel.Channels.pipeline;
+
+public class ThriftPipelineFactory implements ChannelPipelineFactory {
+    private ThriftHandler handler;
+    private int maxFrameSize = 512 * 1024;
+
+    public ThriftPipelineFactory(ThriftHandler handler) {
+        this.handler = handler;
+    }
+
+    public ThriftPipelineFactory(ThriftHandler handler, int maxFrameSize)	{
+        this(handler);
+        this.maxFrameSize = maxFrameSize;
+    }
+
+    public ChannelPipeline getPipeline() throws Exception {
+        ChannelPipeline pipeline = pipeline();
+        pipeline.addLast("frameDecoder", new LengthFieldBasedFrameDecoder(maxFrameSize, 0, 4, 0, 4));
+        pipeline.addLast("frameEncoder", new LengthFieldPrepender(4));
+        pipeline.addLast("thrift-handler", handler);
+        return pipeline;
+    }
+}
