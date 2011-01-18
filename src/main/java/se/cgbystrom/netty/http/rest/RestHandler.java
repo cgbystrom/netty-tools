3db773f889e8fa2a6d085b6c22cf51b5c6b85b07
cgbystrom <cgbystrom@gmail.com>
2011-01-18 13:08:59 +0100

    Handling of before and after annotations

--- src/main/java/se/cgbystrom/netty/http/rest/RestHandler.java
+++ src/main/java/se/cgbystrom/netty/http/rest/RestHandler.java
@@ -10,8 +10,11 @@ import org.jboss.netty.util.CharsetUtil;
 import se.cgbystrom.netty.http.router.Matcher;
 import se.cgbystrom.netty.http.router.Router;
 
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static org.jboss.netty.handler.codec.http.HttpHeaders.isKeepAlive;
@@ -20,6 +23,8 @@ public class RestHandler extends SimpleChannelHandler {
     public Object rest;
     private Router<Method> router = new Router<Method>();
     private Map<Integer, Method> errorHandlers = new HashMap<Integer, Method>();
+    private List<Method> beforeHandlers = new ArrayList<Method>();
+    private List<Method> afterHandlers = new ArrayList<Method>();
 
     public RestHandler(Object rest) {
         this.rest = rest;
@@ -34,6 +39,16 @@ public class RestHandler extends SimpleChannelHandler {
             if (eh != null) {
                 errorHandlers.put(eh.value(), method);
             }
+
+            BeforeHandler bh = method.getAnnotation(BeforeHandler.class);
+            if (bh != null) {
+                beforeHandlers.add(method);
+            }
+
+            AfterHandler ah = method.getAnnotation(AfterHandler.class);
+            if (ah != null) {
+                afterHandlers.add(method);
+            }
         }
     }
 
@@ -42,23 +57,58 @@ public class RestHandler extends SimpleChannelHandler {
         HttpRequest request = (HttpRequest) e.getMessage();
         final Matcher<Method> matcher = router.route(request.getUri());
         HttpResponse response;
-        if (matcher != null) {
-            response = (HttpResponse) matcher.getTarget().invoke(null, request);
-        } else {
-            Method m = errorHandlers.get(404);
-            if (m != null) {
-                response = (HttpResponse)m.invoke(null, request);
+        try {
+            if (matcher != null) {
+                Object o = matcher.getTarget().invoke(null, filterRequest(request));
+                response = filterResponse(o);
             } else {
-                response = default404();
+                Method m = errorHandlers.get(404);
+                if (m != null) {
+                    response = (HttpResponse)m.invoke(null, request);
+                } else {
+                    response = default404();
+                }
             }
+        } catch (RestException re) {
+            response = internalServerError();
         }
 
+
+        e.getChannel().write(response).addListener(ChannelFutureListener.CLOSE);
+    }
+
+    //@Override
+    public void exceptionCaugh1t(ChannelHandlerContext ctx, ExceptionEvent e) throws Exception
+    {
+        HttpResponse response = internalServerError();
         e.getChannel().write(response).addListener(ChannelFutureListener.CLOSE);
     }
 
     private HttpResponse default404() {
         final Response response = new Response(HttpVersion.HTTP_1_0, HttpResponseStatus.NOT_FOUND);
         response.setContent(ChannelBuffers.copiedBuffer("404 Not Found", CharsetUtil.UTF_8));
+        return response;
+    }
+
+    private HttpRequest filterRequest(HttpRequest request) throws InvocationTargetException, IllegalAccessException {
+        HttpRequest filtered = request;
+        for (Method method : beforeHandlers) {
+            filtered = (HttpRequest)method.invoke(null, filtered);
+        }
+        return filtered;
+    }
+
+    private HttpResponse filterResponse(Object response) throws InvocationTargetException, IllegalAccessException {
+        Object filtered = response;
+        for (Method method : afterHandlers) {
+            filtered = method.invoke(null, filtered);
+        }
+        return (HttpResponse)filtered;
+    }
+
+    private HttpResponse internalServerError() {
+        final Response response = new Response(HttpVersion.HTTP_1_0, HttpResponseStatus.INTERNAL_SERVER_ERROR);
+        response.setContent(ChannelBuffers.copiedBuffer("500 Internal Server Error", CharsetUtil.UTF_8));
         return response;
     }
 }
